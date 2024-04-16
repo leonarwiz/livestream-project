3bba28d4ddb280195d347ef4ce63a59fef8b5c54
wizzyLeo <wizzyleo0831@gmail.com>
2024-04-16 21:25:17 +0800

    finish navbar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,9 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <UserButton
-        afterSignOutUrl="/"
-      />
+      Home Page
     </div>
     
   );
