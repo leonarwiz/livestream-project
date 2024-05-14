cb1412862ceba038994112ee080cdbeb4c8397d1
wizzyLeo <wizzyleo0831@gmail.com>
2024-05-14 19:20:09 +0800

    verify the functionality of onBlock and onUnblock

--- actions/block.ts
+++ actions/block.ts
@@ -1,7 +1,10 @@
+"use server";
+
 import { blockUser, unblockUser } from "@/lib/block-service";
 import { revalidatePath } from "next/cache";
 
 export const onBlock = async(id: string) => {
+    console.log("Other User's ID: ", id)
     const blockedUser = await blockUser(id)
     revalidatePath("/")
 
--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { onBlock, onUnblock } from "@/actions/block";
 import { onFollow, onUnfollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
@@ -40,9 +41,22 @@ export const Actions = ({
         }
     }
 
+    const handleBlock = () => {
+        startTransition(() => {
+            onUnblock(userId)
+                .then((data) => toast.success("Unblocked the user"))
+                .catch(() => toast.error("Something went wrong: "))
+        })
+    }
+
     return (
-        <Button disabled={isPending} onClick={onClick} variant="primary">
-            {isFollowing ? "Unfollow" : "Follow"}
-        </Button>
+        <>
+            <Button disabled={isPending} onClick={onClick} variant="primary">
+                {isFollowing ? "Unfollow" : "Follow"}
+            </Button>
+            <Button onClick={handleBlock} disabled={isPending}>
+                Block
+            </Button>
+        </>
     )
 }
