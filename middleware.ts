6487f0332929320a570670d5ce9ec8c21e5ebdc5
wizzyLeo <wizzyleo0831@gmail.com>
2024-05-14 11:12:18 +0800

    add block data scheme

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,9 @@ export default authMiddleware({
   ],
 });
 
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
 };
