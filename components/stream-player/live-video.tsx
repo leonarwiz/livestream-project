329fb9e41c931cbd2247b1d025b7178ad4094e96
wizzyLeo <wizzyleo0831@gmail.com>
2024-05-17 18:38:46 +0800

    add live-video component

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { Participant } from "livekit-client"
+import { Participant, Track } from "livekit-client"
+import { useRef } from "react"
+import {useTracks} from "@livekit/components-react"
 
 interface LiveVideoProps{
     participant: Participant
@@ -9,9 +11,20 @@ interface LiveVideoProps{
 export const LiveVideo = ({
     participant,
 }:LiveVideoProps) => {
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const wrapperRef = useRef<HTMLDivElement>(null)
+
+    useTracks([Track.Source.Camera, Track.Source.Microphone])
+        .filter((track) => track.participant.identity === participant.identity)
+        .forEach((track) => {
+            if(videoRef.current){
+                track.publication.track?.attach(videoRef.current)
+            }
+        })
+
     return (
-        <div>
-            LiveVidoe
+        <div ref={wrapperRef} className="h-full relative flex">
+            <video ref={videoRef} width="100%"/>
         </div>
     )
 }
