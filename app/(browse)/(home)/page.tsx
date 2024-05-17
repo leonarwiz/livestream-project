078951fe83e86696db532792645a4f40f03f459b
wizzyLeo <wizzyleo0831@gmail.com>
2024-05-17 17:53:48 +0800

    create use-viewer-token

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,7 +1,31 @@
+import { StreamPlayer } from '@/components/stream-player'
+import { getUserByUserName } from '@/lib/user-service'
+import { currentUser } from '@clerk/nextjs'
 import React from 'react'
 
-export default function CreatorPage() {
+interface CreatorPageProps{
+  params:{
+    username: string
+  }
+}
+
+export default async function CreatorPage({
+  params
+}:CreatorPageProps) {
+  const externalUser = await currentUser()
+  const user = await getUserByUserName(params.username)
+
+  if(!user || user.externalUserId !== externalUser?.id || !user.stream){
+    throw new Error("Unauthorized")
+  }
+
   return (
-    <div>CreatorPage</div>
+    <div className='w-full'>
+      <StreamPlayer
+        user={user}
+        stream={user.stream}
+        isFollowing={true}
+      />
+    </div>
   )
 }
