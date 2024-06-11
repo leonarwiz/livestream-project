a33c21526469e71b4478c8a4cc58d2f28bf87a74
wizzyLeo <wizzyleo0831@gmail.com>
2024-06-12 00:20:03 +0800

    add home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import {Result, ResultSekeleton} from "./_components/result"
+import { Suspense } from "react";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-4">
-      Home Page
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultSekeleton/>}>
+        <Result/>
+      </Suspense>
     </div>
     
   );
