eb4b9ed818df308f3088d00cf1ceab16ae48d8d6
wizzyLeo <wizzyleo0831@gmail.com>
2024-06-11 23:04:00 +0800

    add about card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,9 +10,13 @@ import { Chat, ChatSekeleton } from "./chat"
 import { ChatToggle } from "./chat-toggle"
 import {Header, HeaderSkeleton} from "./header"
 import {InfoCard} from "./info-card"
+import { AboutCard } from "./about-card"
 
 interface StreamPlayerProps{
-    user: User & {stream: Stream | null}
+    user: User & {
+        stream: Stream | null,
+        _count: {followedBy: number}
+    }
     stream: Stream
     isFollowing: boolean
 }
@@ -71,6 +75,13 @@ export const StreamPlayer = ({
                         viewerIdentity={identity}
                         name={name}
                         thumbnailUrl = {stream.thumbnailUrl}
+                    />
+                    <AboutCard 
+                        hostName={user.username}
+                        hostIdentity={user.id}
+                        viewerIdentity={identity}
+                        bio={user.bio}
+                        followedByCount={user._count.followedBy}
                     />
                 </div>
                 <div
